3a4a3a95bf2348daba33a4984fa8e79f69184efc
Soham Srimany <73331793+soham0511@users.noreply.github.com>
2021-06-21 15:35:47 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from "firebase";
 const firebaseConfig = {
-    apiKey: "AIzaSyAbmZq2_onvufT5Q0eOBm_y7BSSbBx9Rxc",
-    authDomain: "twitter-clone-a14ff.firebaseapp.com",
-    projectId: "twitter-clone-a14ff",
-    storageBucket: "twitter-clone-a14ff.appspot.com",
-    messagingSenderId: "764323288566",
-    appId: "1:764323288566:web:14762f630ea136e78263d7",
-    measurementId: "G-J1KFMJQHXY"
+    apiKey: "X",
+    authDomain: "X",
+    projectId: "X",
+    storageBucket: "X",
+    messagingSenderId: "X",
+    appId: "X:X:X",
+    measurementId: "X"
   };
   const firebaseApp = firebase.initializeApp(firebaseConfig);
 
